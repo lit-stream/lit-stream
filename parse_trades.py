37469fc6f44abcfba109b7cc3e223bb277ac3ead
Lit Stream <sb0ilwx4@duck.com>
2024-06-27 00:50:42 +0300

    wip

--- parse_trades.py
+++ parse_trades.py
@@ -1,57 +1,49 @@
-###!/usr/bin/python3
-
+#from flask import Flask, render_template
 from decimal import Decimal as Dec
+from requests import Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
 
+import config as cfg
 
-THEADER = "#asset,transaction date,transaction type,coin amount,EUR,fee,PricePerUnit"
+DATA = cfg.DATA
 
+def coin_price(coins):
+    url = cfg.url
+    coins = ','.join(coins)
+    
+    parameters = {
+        'symbol':coins,
+        'convert':'EUR'
+    }
+    headers = {
+      'Accepts': 'application/json',
+      'X-CMC_PRO_API_KEY': cfg.api_key,
+    }
+    session = Session()
+    session.headers.update(headers)
+    try:
+        response = session.get(url, params=parameters)
+        return json.loads(response.text)
+        #return (data['data'][coin]['quote']['EUR']['price'])
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+        return e
 
 def read_csv():
-    data = []
+    csv_data = []
     f = open("ledgers.csv", "r")
     lines = f.readlines()
     for line in lines:
-        data.append(line.replace('"',''))
-    return data
-
+        csv_data.append(line.replace('"',''))
+    return csv_data
 
 def extract_ticker(ticker):
-    if ticker in {"ADA", "ADA.S"}:
-        return "ADA"
-    if ticker in {"DOT", "DOT.S"}:
-        return "DOT"
-    if ticker in {"ZEUR", "EUR.HOLD", "EUR"}:
-        return "EUR"
-    if ticker in {"XXBT", "BTC"}:
-        return "BTC"
-    if ticker in {"XETH", "ETH"}:
-        return "ETH"
-    if "LRC" in ticker:
-        return "LRC"
-    if ticker in {"XXRP", "XRP"}:
-        return "XRP"
-    if ticker in {"XXLM", "XLM"}:
-        return "XLM"
-    if ticker in {"XXDG", "DOGE"}:
-        return "DOGE"
-    if "CHZ" in ticker:
-        return "CHZ"
-    if "LINK" in ticker:
-        return "LINK"
-    if "MATIC" in ticker:
-        return "MATIC"
-    if "ETHW" in ticker:
-        return "ETHW"
-    if ticker in {"RNDR"}:
-        return "RNDR"
-    if ticker in {"PEPE"}:
-        return "PEPE"
-    if ticker in {"ONDO"}:
-        return "ONDO"
-    return "TICKER"
-
-def sort_data(data):
-    return sorted(data, key=lambda x:x[:3])
+    for coin in DATA.keys():
+        if ticker in DATA[coin]["ticker"]:
+            return coin
+    else:
+        return "unknown"
 
 def ppu(amount, cost, ticker):
     if ticker in {"PEPE"}:
@@ -59,17 +51,34 @@ def ppu(amount, cost, ticker):
     return "%0.2f" % (abs(cost)/abs(amount))
 
 def purchase_sale(line1, line2):
-    if extract_ticker(line1[6]) == "EUR":
-        # purchase
-        return f"{extract_ticker(line2[6])},{line2[2]},purchase,{Dec(line2[8])},{Dec(line1[8])},{Dec(line1[9])}," \
-               f"{ppu(Dec(line2[8]), Dec(line1[8]), extract_ticker(line2[6]))}"
-    # sale
-    return f"{extract_ticker(line1[6])},{line2[2]},sale,{Dec(line1[8])},{Dec(line2[8])},{Dec(line2[9])}," \
-           f"{ppu(Dec(line1[8]), Dec(line2[8]), extract_ticker(line2[6]))}"
+    line1_ticker = extract_ticker(line1[6])
+    line2_ticker = extract_ticker(line2[6])
+    date = line2[2]
+    p_price = ppu(Dec(line2[8]), Dec(line1[8]), line2_ticker)
+    s_price = ppu(Dec(line1[8]), Dec(line2[8]), line1_ticker)
+    print(DATA)
 
+    if line1_ticker == "EUR": # purchase
+        DATA[line2_ticker]["purchase"].append({"date": date,
+                                               "amount": Dec(line2[8]),
+                                               "price": Dec(line1[8]),
+                                               "ppu": p_price,
+                                               "fee": Dec(line1[9])
+                                               })
+        DATA["EUR"]["total_fee"] += round(Dec(line1[9]), 4)
+        return True
+    # sale
+    DATA[line1_ticker]["sale"].append({"date": date,
+                                       "amount": Dec(line1[8]),
+                                       "price": Dec(line2[8]),
+                                       "ppu": s_price,
+                                       "fee": Dec(line2[9])
+                                       })
+    DATA["EUR"]["total_fee"] += round(Dec(line2[9]), 4)
+    print(DATA)
+    return True
 
 def parse_trades():
-    data = [THEADER]
     data_lines = read_csv()
     del data_lines[0]
     for i, line1 in enumerate(data_lines):
@@ -82,28 +91,37 @@ def parse_trades():
         if line1s[3] == "withdrawal":
             continue
         if line1s[3] == "deposit" and ticker1 == "EUR":
-            data.append(f"{ticker1},{line1s[2]},deposit,0,{Dec(line1s[8])},0")
+            DATA[ticker1]["deposit"].append({"date": line1s[2],
+                                             "amount": Dec(line1s[8])
+                                             })
+            DATA["EUR"]["total_deposits"] = Dec(DATA["EUR"]["total_deposits"]) + Dec(line1s[8])
             continue
         if line1s[3] == "staking" or (line1s[3] == "earn" and line1s[4] == "reward"):
-            data.append(f"{ticker1},{line1s[2]},staking,{Dec(line1s[8])},0,{Dec(line1s[9])}")
+            DATA[ticker1]["staking"]["date"] = line1s[2]
+            DATA[ticker1]["staking"]["amount"] += round(Dec(line1s[8]), 10)
+            DATA[ticker1]["staking"]["fee"] += round(Dec(line1s[9]), 10)
+            continue
+        if line1s[3] == "transfer" and line1s[4] == "spotfromfutures":
+            DATA[ticker1]["purchase"].append({"date": line1s[2],
+                                              "amount": Dec(line1s[8]),
+                                              "price": 0,
+                                              "ppu": 0,
+                                              "fee": 0
+                                              })
             continue
         for line2 in data_lines[i+1:]:
             line2s = line2.split(",")
             if len(line2s) < 5:
-                break
+                continue
             if line1s[1] == line2s[1]:
                 if line1s[3] in {"spend", "trade"} and line2s[3] in {"receive", "trade"}:
-                    data.append(purchase_sale(line1s, line2s))
-                    break
-                if line1s[3] == "deposit" and line2s[4] == "spotfromfutures":
-                    data.append(f"{extract_ticker(line2s[6])},{line2s[2]},deposit,{Dec(line2s[8])},0,{Dec(line2s[9])}")
+                    purchase_sale(line1s, line2s)
                     break
             if line2s[3] == "withdrawal":
-                break
-    #return sort_data(data)
-    return data
+                continue
 
-def cleanup_staking(data):
+
+'''def cleanup_staking(data):
     clean_data = []
     for item in data:
         if "staking" in item:
@@ -123,12 +141,8 @@ def cleanup_staking(data):
         else:
             clean_data.append(item)
     return clean_data
+'''
 
+parse_trades()
+#print(DATA)
 
-trades = parse_trades()
-#for item in trades:
-#    print(item)
-
-new_data = cleanup_staking(trades)
-for item in new_data:
-    print(item)
